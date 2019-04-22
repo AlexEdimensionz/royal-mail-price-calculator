2f159e0e2cd96bf887002f5f78064c9ae7ee9934
crypto <alex@e-dimensionz.com>
2019-04-22 04:07:21 +0300

    March 2018 - Price Data update

--- src/Services/InternationalService.php
+++ src/Services/InternationalService.php
@@ -10,6 +10,7 @@ abstract class InternationalService extends Service
 	const LETTER = 'letter';
 	const LARGE_LETTER = 'large_letter';
 	const SMALL_PARCEL = 'small_parcel';
+	const MEDIUM_PARCEL = 'medium_parcel';
 	const TUBE = 'tube';
 
 	/**
@@ -32,6 +33,12 @@ abstract class InternationalService extends Service
 
 		} elseif ($length <= 35.3 && $width <= 25 && $depth <= 2.5 && $weight <= 750) {
 			return self::LARGE_LETTER;
+
+		} elseif ($length <= 45 && $width <= 35 && $depth <= 16 && $weight <= 2000) {
+			return self::SMALL_PARCEL;
+
+		} elseif ($length <= 61 && $width <= 46 && $depth <= 46 && $weight <= 20000) {
+			return self::MEDIUM_PARCEL;
 
 		} elseif ($length + $depth + $width <= 90 && $length <= 60 && $width <= 60 && $depth <= 60 && $weight <= 2000) {
 			return self::SMALL_PARCEL;
